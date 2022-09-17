8598f0bc416b9ccd337c8b6c5b8f427dc83c27c2
r.yakimenko <r.yakimenko.73@gmail.com>
2022-09-17 23:57:38 +0300

    settings/urls: zalupa

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include, re_path
 from django.views.static import serve
 
@@ -9,6 +10,8 @@ urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('logs/', include('logs.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 if not settings.SERVER_SETTINGS.debug:
     urlpatterns += re_path(
